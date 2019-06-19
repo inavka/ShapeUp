aef9f8f2f0d4392180d9aad11c4c06b0b72297d2
inavka <ivanka.zhuravchak@gmail.com>
2019-06-19 21:28:18 +0300

    reviews slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,35 @@
 $(document).ready(function () {
+
+    var scroll_start = 0;
+    var startchange = $('#startchange');
+    var offset = startchange.offset();
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $(".navbar").css('background-color', '#161616');
+            } else {
+                $('.navbar').css('background-color', 'transparent');
+            }
+        });
+    }
+
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+
+
     $('.main-slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -6,6 +37,8 @@ $(document).ready(function () {
         fade: true,
         asNavFor: '.sync-slider'
     });
+
+
     $('.sync-slider').slick({
         slidesToShow: 3,
         slidesToScroll: 0,
@@ -15,4 +48,31 @@ $(document).ready(function () {
         focusOnSelect: true,
         infinite: true
     });
+
+
+    $('.reviews__slider').slick({
+        mobileFirst:true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
+
